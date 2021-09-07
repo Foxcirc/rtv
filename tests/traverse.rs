c417d9d0a8f349b50e9783ceb48e6eafbe1394b6
foxcirc <whyfoxcirc@gmx.de>
2021-09-07 22:40:59 +0200

    fix tests finally

--- tests/traverse.rs
+++ tests/traverse.rs
@@ -18,6 +18,7 @@ fn traverse() -> io::Result<()> {
         file.read_to_string(&mut buff2).unwrap();
     }).for_each(drop);
 
+    // the '||' is for github actions wich uses ubuntu instead of windows
     assert!((&buff == "yes\no world!yes\nyes\nno\nyes\nhehe│\r\ncomputer\r\n│" || &buff == "yes\r\no world!yes\r\nyes\r\nno\r\nyes\r\nhehe│\r\ncomputer\r\n│"));
     assert!(buff == buff2);
 
