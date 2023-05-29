d9f44804c4861a44bea18a6a25f37a040d653a79
foxcirc <whyfoxcirc@gmx.de>
2023-05-29 11:14:23 +0200

    added some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,46 @@
 
+//! # Rtv
+//!
+//! Rtv is a crate to "just execute" a future, on a single thread,
+//! without any additional dependencies.
+//! This crate tries to avoid unsafe as much as possible.
+//! You can easily view all of the source code as it is all contained within `lib.rs`
+//!
+//! There are two available functions:
+//! - [`execute`] to simplty execute a future
+//! - [`timeout`] to execute a future with a given timoeut
+//!
+
 use std::{future::Future, task::{Context, Waker, Poll}, sync::mpsc::{sync_channel, RecvTimeoutError}, time::{Duration, Instant}};
 
 #[cfg(test)]
-
 mod test;
 
+/// execute a future
+///
+/// # Example
+///
+/// ```rust
+/// let future = std::future::ready(69);
+/// let result = rtv::execute(future);
+/// assert!(result == 69);
+/// ```
 #[inline]
 pub fn execute<T>(future: impl Future<Output = T> + Unpin) -> T {
     run(future, Duration::MAX).expect("Duration::MAX elapsed")
 }
 
+/// execute a future with given timeout
+///
+/// # Example
+///
+/// This code will finish after `2` seconds.
+///
+/// ```rust
+/// let future = std::future::pending();
+/// let result: Option<()> = rtv::timeout(future, std::time::Duration::from_secs(2));
+/// assert!(result == None)
+/// ```
 #[inline]
 pub fn timeout<T>(future: impl Future<Output = T> + Unpin, timeout: Duration) -> Option<T> {
     run(future, timeout)
@@ -46,7 +77,7 @@ mod waker {
 
     use std::{task::{RawWaker, RawWakerVTable}, sync::mpsc::SyncSender, mem::ManuallyDrop};
 
-    const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+    const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, waker_drop);
     type WakerSender = ManuallyDrop<SyncSender<()>>;
 
     pub(crate) fn new(sender: SyncSender<()>) -> RawWaker {
@@ -61,14 +92,14 @@ mod waker {
 
     unsafe fn wake(data: *const ()) {
         wake_by_ref(data);
-        drop(data);
+        waker_drop(data);
     }
 
     unsafe fn wake_by_ref(data: *const ()) {
         sender(data).send(()).expect("Channel disconnected")
     }
 
-    unsafe fn drop(data: *const ()) {
+    unsafe fn waker_drop(data: *const ()) {
         ManuallyDrop::drop(sender(data))
     }
 
